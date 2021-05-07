53ca8a3dd65c169c8f1ef98be85d8dd7fb13eb5b
Alexis Goup <alexis.goupillot.dev@gmail.com>
2021-05-07 16:24:10 +0200

    Finished cards indicators weather in Home section

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PrevisionDatas } from 'src/app/model/PrevisionDatas';
 import { Town } from 'src/app/model/town';
 import { TownData } from 'src/app/model/TownData';
 import { DataAccessService } from 'src/app/services/dao-api/data-access.service';
@@ -12,18 +13,24 @@ export class HomeComponent implements OnInit {
 
   currentCity: any = {} as Town
   townDatas: any = {} as TownData
+  previsionDatas: any = {} as PrevisionDatas[]
 
   constructor(private dataAccess: DataAccessService) { }
 
   ngOnInit(): void {
     this.currentCity = 'Montpellier'
     this.getTownData()   
+    this.getPrevisionTownData()
   }
 
   async getTownData() {
     this.townDatas = await this.dataAccess.getDataByCityNameForPastHours(this.currentCity, 1).toPromise()
-    console.log(this.townDatas)
-    
+    console.log(this.townDatas)   
+  }
+
+  async getPrevisionTownData() {
+    this.previsionDatas = await this.dataAccess.getDataByCityNameForNextDays(this.currentCity).toPromise()
+    console.log(this.previsionDatas)
   }
 
 }
