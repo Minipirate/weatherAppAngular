948a13e8477d02d90f8e1b49463fae0a2193e172
Alexis Goup <alexis.goupillot.dev@gmail.com>
2021-05-08 16:48:47 +0200

    Updated Home (dynamic background, icons,...)

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -3,6 +3,7 @@ import { PrevisionDatas } from 'src/app/model/PrevisionDatas';
 import { Town } from 'src/app/model/town';
 import { TownData } from 'src/app/model/TownData';
 import { DataAccessService } from 'src/app/services/dao-api/data-access.service';
+import { PrevisionService } from 'src/app/services/prevision/prevision.service';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +13,11 @@ import { DataAccessService } from 'src/app/services/dao-api/data-access.service'
 export class HomeComponent implements OnInit {
 
   currentCity: any = {} as Town
+  currentDay:any = {} as string
   townDatas: any = {} as TownData
   previsionDatas: any = {} as PrevisionDatas[]
 
-  constructor(private dataAccess: DataAccessService) { }
+  constructor(private dataAccess: DataAccessService, private previsionService: PrevisionService) { }
 
   ngOnInit(): void {
     this.currentCity = 'Montpellier'
@@ -25,12 +27,18 @@ export class HomeComponent implements OnInit {
 
   async getTownData() {
     this.townDatas = await this.dataAccess.getDataByCityNameForPastHours(this.currentCity, 1).toPromise()
-    console.log(this.townDatas)   
+    console.log(this.townDatas);
+    
+    this.findDayByDate() // Appel apres recuperation de la date du jour 
   }
 
   async getPrevisionTownData() {
     this.previsionDatas = await this.dataAccess.getDataByCityNameForNextDays(this.currentCity).toPromise()
-    console.log(this.previsionDatas)
+  }
+
+  findDayByDate() {
+    let date = this.previsionService.frenchToAmericanDate(this.townDatas[0].dateHour)
+    this.currentDay = this.previsionService.whatDayIsIt(new Date(date).getDay()) 
   }
 
 }
--- src/app/home/weather/indicator-weather/indicator-weather.component.ts
+++ src/app/home/weather/indicator-weather/indicator-weather.component.ts
@@ -1,74 +1,24 @@
-import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { PrevisionDatas } from 'src/app/model/PrevisionDatas';
+import { PrevisionService } from 'src/app/services/prevision/prevision.service';
 
 @Component({
   selector: 'app-indicator-weather',
   templateUrl: './indicator-weather.component.html',
-  styleUrls: ['./indicator-weather.component.css'],
-  providers: [DatePipe]
+  styleUrls: ['./indicator-weather.component.css']
 })
 export class IndicatorWeatherComponent implements OnInit {
 
   @Input() previsionDatas: any = {} as PrevisionDatas
-  dataArray: any 
   myDates: string[] = []
   myDays: string[] = []
 
-  constructor(private datePipe: DatePipe) { 
+  constructor(private prevision: PrevisionService) { 
   }
 
   ngOnInit(): void {
-    this.populateDates()
-  }
-
-  ngOnChanges() {
-    this.previsionsDatasToArray()
-  }
-
-  previsionsDatasToArray() {
-    if (this.previsionDatas === undefined) return
-
-  }
-
-  populateDates() {
-    let currentDay = new Date().getDay()
-
-    for (let i = 1; i < 4; i++) {
-      let date : string = this.datePipe.transform(new Date().setDate(new Date().getDate() + i), 'dd-MM-yyyy') + ' 11:00'  
-      this.myDates.push(date)   
-      this.myDays.push(this.whatDayIsIt(currentDay + i)) 
-    }
-
-    console.log(this.myDates);
-    console.log(this.myDays)
-  }
-
-  whatDayIsIt(day: number) :string {
-
-    // Jugez pas svp <3
-    if (day == 7) day = 0
-    if (day == 8) day = 1
-    if (day == 9) day = 2 
-
-    switch (day) {
-      case 0:
-        return "Dimanche";
-      case 1:
-        return "Lundi"
-      case 2:
-        return "Mardi";
-      case 3:
-        return "Mercredi";
-      case 4:
-        return "Jeudi";
-      case 5:
-        return "Vendredi";
-      case 6:
-        return "Samedi";
-      default:
-        return ''
-    }
+    this.myDates = this.prevision.populateDates(3)
+    this.myDays = this.prevision.populateDays(3)
   }
 
 }
